a6f26f0bdcd4edb0e674314edf980225bd2378ed
praveenvijayan <praveenv.vijayan@gmail.com>
2013-07-19 23:21:18 +0530

    Updated test

--- tasks/html_validation.js
+++ tasks/html_validation.js
@@ -6,124 +6,124 @@
  * Licensed under the MIT license.
  */
 
- 'use strict';
+'use strict';
 
- module.exports = function(grunt) {
+module.exports = function(grunt) {
 
-  var w3cjs = require('w3cjs');
-  var colors = require('colors');
-  var fs = require('fs');
+	var w3cjs = require('w3cjs');
+	var colors = require('colors');
+	var fs = require('fs');
 
-  colors.setTheme({
-    silly: 'rainbow',
-    input: 'grey',
-    verbose: 'cyan',
-    prompt: 'grey',
-    info: 'green',
-    data: 'grey',
-    help: 'cyan',
-    warn: 'yellow',
-    debug: 'blue',
-    error: 'red',
-    blue: 'blue'
-  });
+	colors.setTheme({
+		silly: 'rainbow',
+		input: 'grey',
+		verbose: 'cyan',
+		prompt: 'grey',
+		info: 'green',
+		data: 'grey',
+		help: 'cyan',
+		warn: 'yellow',
+		debug: 'blue',
+		error: 'red',
+		blue: 'blue'
+	});
 
-  var htmlContent = "",
-  arryFile=[],
-  counter = 0,
-  msg={
-    error: "Something went wrong",
-    ok: "Validation successful..",
-    start: "Validation started for.. ",
-    networkError: 'Network error re-validating..',
-    validFile:"Validated skipping.."
-  },
-  len,
-  fileStat = {},
-  isModified,
-  fileCount = 0,
-  validsettings="";
-  
-  grunt.registerMultiTask('validation', 'HTML W3C validation.', function() {
-    // Merge task-specific and/or target-specific options with these defaults.
-    var options = this.options({
-      path: "validation-staus.json",
-      reset:false
-    });
-    
-    var done = this.async(),
-    files = grunt.file.expand(this.filesSrc),
-    flen = files.length,
-    readSettings = {};
+	var htmlContent = "",
+		arryFile = [],
+		counter = 0,
+		msg = {
+			error: "Something went wrong",
+			ok: "Validation successful..",
+			start: "Validation started for.. ",
+			networkError: 'Network error re-validating..',
+			validFile: "Validated skipping.."
+		},
+		len,
+		fileStat = {},
+		isModified,
+		fileCount = 0,
+		validsettings = "";
 
-    if(options.reset){
-      grunt.file.write(options.path, '{}');
-    }
+	grunt.registerMultiTask('validation', 'HTML W3C validation.', function() {
+		// Merge task-specific and/or target-specific options with these defaults.
+		var options = this.options({
+			path: "validation-staus.json",
+			reset: false
+		});
 
-    var validate = function (files) {
-      if(files.length){
-        if(grunt.file.exists(options.path)){
-          readSettings = grunt.file.readJSON(options.path);
-        }
-        var currFileStat = readSettings[files[counter]] || false;
+		var done = this.async(),
+			files = grunt.file.expand(this.filesSrc),
+			flen = files.length,
+			readSettings = {};
 
-        if(currFileStat){
-          console.log(msg.validFile.green + files[counter]);
-          counter++;
-          validate(files);
-          return;
-        }
+		if (options.reset) {
+			grunt.file.write(options.path, '{}');
+		}
 
-        console.log(msg.start+files[counter]);
+		var validate = function(files) {
+			if (files.length) {
+				if (grunt.file.exists(options.path)) {
+					readSettings = grunt.file.readJSON(options.path);
+				}
+				var currFileStat = readSettings[files[counter]] || false;
 
-        var results = w3cjs.validate({
-                      file: files[counter], // file can either be a local file or a remote file
-                      //file: 'http://html5boilerplate.com/',
-                      output: 'json', // Defaults to 'json', other option includes html
-                      callback: function (res) {
+				if (currFileStat) {
+					console.log(msg.validFile.green + files[counter]);
+					counter++;
+					validate(files);
+					return;
+				}
 
-                        if(!res.messages){
-                          console.log(msg.networkError.error)
-                          validate(files);
-                          return;
-                        }
+				console.log(msg.start + files[counter]);
 
-                        len = res.messages.length;
+				var results = w3cjs.validate({
+					file: files[counter], // file can either be a local file or a remote file
+					//file: 'http://html5boilerplate.com/',
+					output: 'json', // Defaults to 'json', other option includes html
+					callback: function(res) {
 
-                        if(len){
+						if (!res.messages) {
+							console.log(msg.networkError.error);
+							validate(files);
+							return;
+						}
 
-                          for(var prop in res.messages){
-                            console.log(prop + "=> ".warn +JSON.stringify(res.messages[prop].message).help + 
-                              " Line no: " + JSON.stringify(res.messages[prop].lastLine).prompt
-                              );
-                          }
+						len = res.messages.length;
 
-                          readSettings[files[counter]] = false;
-                          console.log("No of errors: ".error + res.messages.length);
+						if (len) {
 
-                        }else{
+							for (var prop in res.messages) {
+								console.log(prop + "=> ".warn + JSON.stringify(res.messages[prop].message).help +
+									" Line no: " + JSON.stringify(res.messages[prop].lastLine).prompt
+								);
+							}
 
-                          readSettings[files[counter]] = true;
-                          grunt.log.ok(msg.ok.green);
+							readSettings[files[counter]] = false;
+							console.log("No of errors: ".error + res.messages.length);
 
-                        }
+						} else {
 
-                        grunt.file.write(options.path, JSON.stringify(readSettings));
+							readSettings[files[counter]] = true;
+							grunt.log.ok(msg.ok.green);
 
-                          // depending on the output type, res will either be a json object or a html string
-                          // console.log(arryFile);
-                          counter++;
-                          if(counter === flen-1){
-                            done();
-                          }
-                          validate(files);
-                        }
-                      });          
-}
-}
+						}
 
-validate(files);
+						grunt.file.write(options.path, JSON.stringify(readSettings));
 
-});
+						// depending on the output type, res will either be a json object or a html string
+						// console.log(arryFile);
+						counter++;
+						if (counter === flen - 1) {
+							done();
+						}
+						validate(files);
+					}
+				});
+			}
+		};
+
+		validate(files);
+
+	});
 
 };
